fb34b9cb0969c27d2c60c05b1ad9c1dbc4171039
dola.chakraborty@kohler.com <dola.chakraborty@kohler.com>
2023-06-28 12:14:52 +0530

    fix: font size text content

--- src/components/textContent/index.js
+++ src/components/textContent/index.js
@@ -3,7 +3,7 @@ import { RichText } from '@components/RichText';
 const TextContent = ({ heading, content }) => {
   return (
     <div className='max-w-screen-lg mx-auto px-8 my-12 pb-3'>
-      <h2 className='mt-[20px] mb-[30px] text-[2rem] md:text-[5rem] font-normal text-black'>
+      <h2 className='mt-[20px] mb-[30px] text-[2rem] md:text-[2.7rem] font-normal text-black'>
         {heading}
       </h2>
       {content?.map((sect, index) => (
