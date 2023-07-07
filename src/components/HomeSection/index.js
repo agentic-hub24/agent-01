31bebd19dd389b84a34916d357d9b5dd0b4e99a5
Afzal Ahmad <AFZAL.AHMAD@kohler.com>
2023-07-07 15:41:11 +0530

    literature page added

--- src/components/HomeSection/index.js
+++ src/components/HomeSection/index.js
@@ -1,12 +1,17 @@
 import ArticleCard from '@components/cards/ArticleCard';
 
 const HomeSection = ({ homePageData }) => {
+  const isLiterature = homePageData?.internalTitle.includes('Literature');
   return (
     <section className='bg-[#e3e3e3]'>
       <div className='grid grid-cols-1 w-full mx-auto max-w-screen-lg'>
-        <div className='text-center flex justify-center flex-wrap'>
+        <div className='text-center flex mx-10 md:mx-16 lg:mx-8 flex-wrap'>
           {homePageData?.cardRows?.map(({ fields, sys }) => (
-            <ArticleCard key={sys?.id} cardRows={fields?.cardItem} />
+            <ArticleCard
+              key={sys?.id}
+              cardRows={fields?.cardItem}
+              isLiterature={isLiterature}
+            />
           ))}
         </div>
       </div>
