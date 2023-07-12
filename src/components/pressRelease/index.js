1cdfb320e2cd7db8adcb21d768619fc46a0c2992
Afzal Ahmad <AFZAL.AHMAD@kohler.com>
2023-07-12 20:14:59 +0530

    WIP press release tabs work

--- src/components/pressRelease/index.js
+++ src/components/pressRelease/index.js
@@ -1,74 +1,131 @@
-import React, { useState } from 'react';
+import React, { useState, memo } from 'react';
+import Cta from '@components/Cta';
 import { RichText } from '@components/RichText';
 
 const PressRelease = ({ pageData }) => {
-  console.log('page data ', pageData);
+  console.log('dd ', pageData);
   const { pageHeading, pageSections } = pageData;
+  // Preparing tabs button exluding bottom help us link
   const ctaButtons = pageSections.filter(
-    item => item?.sys?.contentType?.sys?.id === 'cta'
+    item =>
+      item?.sys?.contentType?.sys?.id === 'cta' &&
+      !item?.fields.internalTitle.includes('Help Us Improve')
   );
+
+  // Preparing Bottom help us link
+  const helpUsLink = pageSections.find(item =>
+    item?.fields.internalTitle.includes('Help Us Improve')
+  );
+
+  // Filtering tabs content data
   const textHeadingImageContentSection = pageSections.filter(
     item => item?.sys?.contentType?.sys?.id === 'textHeadingImageContentSection'
   );
+
+  // preparing default tabs data which will loaded first
+  const defaultTabData = textHeadingImageContentSection.filter(item =>
+    item.fields.internalTitle.includes('Recent')
+  );
+
+  const [activeButton, setActiveButton] = useState(
+    ctaButtons?.[0].fields?.internalTitle
+  );
+  const [tabsData, setTabsData] = useState(defaultTabData);
 
   const getFormattedDate = d => {
     const date = new Date(d);
     const month = date.toLocaleString('default', { month: 'long' });
     return `${month} ${date.getDate()}, ${date.getFullYear()}`;
   };
-  console.log('ctaButtons ', ctaButtons);
+
+  const handleTabs = btnIntTitle => {
+    setActiveButton(btnIntTitle);
+    createTabData(btnIntTitle);
+  };
+
+  const handleSelect = e => {
+    const selectedVal = e.target.value;
+    createTabData(selectedVal);
+  };
+
+  // Preparing tab data based on button clicked
+  const createTabData = btnIntTitle => {
+    let title = 'Press Releases';
+    if (btnIntTitle.includes('CTA 1')) {
+      title = 'Recent';
+    }
+    const data = textHeadingImageContentSection.filter(item =>
+      item.fields.internalTitle.includes(title)
+    );
+    setTabsData(data);
+  };
   return (
-    <section className='px-4 lg:px-0'>
-      <div className='grid grid-cols-1 w-full mx-auto max-w-screen-lg'>
-        <div className='text-center py-12'>
-          <h1 className='text-[32px] text-[#232323] font-[700] mb-8'>
-            {pageHeading}
-          </h1>
-          <div className='hidden lg:block'>
-            {ctaButtons?.map(
-              ({ fields }) =>
-                fields?.visible && (
-                  <button
-                    key={fields?.label}
-                    className='text-[18px] text-[#fff] bg-[#232323] font-[500] py-2 px-4 rounded-md mr-4'
-                  >
-                    {fields?.label}
-                  </button>
-                )
-            )}
-          </div>
-          <div className='block lg:hidden'>
-            <select className='w-[50%] border-solid border-2 border-grey'>
+    <>
+      <section className='px-4 lg:px-0'>
+        <div className='grid grid-cols-1 w-full mx-auto max-w-screen-lg'>
+          <div className='text-center py-12'>
+            <h1 className='text-[32px] text-[#232323] font-[700] mb-8'>
+              {pageHeading}
+            </h1>
+            <div className='hidden lg:block'>
               {ctaButtons?.map(
                 ({ fields }) =>
                   fields?.visible && (
-                    <option key={fields?.label} className='text-[12px]'>
+                    <button
+                      key={fields?.label}
+                      className={`text-[18px] font-[500] py-2 px-4 rounded-md mr-4 ${
+                        activeButton === fields?.internalTitle
+                          ? 'bg-[#232323]  text-[#fff]'
+                          : 'bg-[#e5e5e5] text-[#000]'
+                      }`}
+                      onClick={() => handleTabs(fields?.internalTitle)}
+                    >
                       {fields?.label}
-                    </option>
+                    </button>
                   )
               )}
-            </select>
+            </div>
+            <div className='block lg:hidden'>
+              <select
+                className='w-[50%] border-solid border-2 border-grey'
+                onChange={handleSelect}
+              >
+                {ctaButtons?.map(
+                  ({ fields }) =>
+                    fields?.visible && (
+                      <option
+                        key={fields?.label}
+                        className='text-[12px]'
+                        value={fields?.internalTitle}
+                      >
+                        {fields?.label}
+                      </option>
+                    )
+                )}
+              </select>
+            </div>
           </div>
+          {tabsData.map(({ fields }, index) => (
+            <div
+              key={index}
+              className={`${
+                index === 0
+                  ? 'bg-[#e5e5e5] p-4 mb-8'
+                  : 'border-solid border-b-2 border-grey mb-10'
+              }`}
+            >
+              {fields?.date && <span>{getFormattedDate(fields?.date)}</span>}
+              <RichText
+                text={fields?.textContent}
+                paragraphCustomClasses='text-[14px] text-black'
+              />
+            </div>
+          ))}
         </div>
-        {textHeadingImageContentSection.map(({ fields }, index) => (
-          <div
-            key={index}
-            className={`${
-              index === 0
-                ? 'bg-[#e5e5e5] p-4 mb-8'
-                : 'border-solid border-b-2 border-grey mb-10'
-            }`}
-          >
-            {fields?.date && <span>{getFormattedDate(fields?.date)}</span>}
-            <RichText
-              text={fields?.textContent}
-              paragraphCustomClasses='text-[14px] text-black'
-            />
-          </div>
-        ))}
-      </div>
-    </section>
+      </section>
+      <Cta fields={helpUsLink?.fields} />
+    </>
   );
 };
 
-export default PressRelease;
+export default memo(PressRelease);
