ed9cdf181b2b76daf9238e95623c60695a34fb4a
Kushal Arora <KUSHAL.ARORA@kohler.com>
2023-07-21 17:36:50 +0530

    BM-1067: categories response restructuring

--- src/pages/api/categories.js
+++ src/pages/api/categories.js
@@ -20,8 +20,12 @@ const categories = async (req, res) => {
                 "top":1000,
                 "skip":0,
                 "search":"*",
-                "filter":""
+                "filter":"",
+                "select":process.env.Common_Fields
             }
+            
+            bodyData["select"] += (req.body.lang==="en") ? "," + process.env.English_Fields : "," + process.env.Spanish_Fields; 
+
             let regionMainCategory = (req.body.lang==="en") ? "RegionSubCategory" : "RegionSubCategory_esMX";
             let regionProductCategory = (req.body.lang==="en") ? "RegionProductCategoryLocal" : "RegionProductCategoryLocal_esMX";
 
@@ -83,10 +87,59 @@ const categories = async (req, res) => {
                     error: 'There was an error'
                 });
             }
-
-            if(response['@search.facets']['ProductLocalCategory_esMX']){
-                response['subCategoryCount'] = response['@search.facets']['ProductLocalCategory_esMX'].length;
+            
+            // conversion of keys
+            if(req.body.lang !== "en" && response.value.length > 0){
+                for (let i = 0; i < response.value.length; i++) {
+                    const iterator = response.value[i];
+                    const {ProductDescriptionProductShort_esMX,ProductLocalCategory_esMX,SKUColorFinishName_esMX,ProductMaterial_esMX,ProductProductType_esMX,ProductATGDefaultCategory_esMX,ProductSection_esMX,ProductInstallationType_esMX,ProductShape_esMX,SprayType_esMX,NightLight_esMX,NumberOfHoles_esMX,ProductConfiguration_esMX,IntelligentToilet_esMX,BowlShape_esMX,HandleStyle_esMX,BidetFunctionality_esMX,TouchlessFaucet_esMX,RegionProductCategoryLocal_esMX,RegionSubCategory_esMX } = iterator;
+                    const modifiedObj = {
+                        ProductDescriptionProductShort:ProductDescriptionProductShort_esMX,ProductLocalCategory: ProductLocalCategory_esMX,
+                        SKUColorFinishName:SKUColorFinishName_esMX,
+                        ProductMaterial:ProductMaterial_esMX,
+                        ProductProductType:ProductProductType_esMX,
+                        ProductATGDefaultCategory:ProductATGDefaultCategory_esMX,
+                        ProductSection:ProductSection_esMX,
+                        ProductInstallationType:ProductInstallationType_esMX,
+                        ProductShape:ProductShape_esMX,
+                        SprayType:SprayType_esMX,
+                        NightLight:NightLight_esMX,
+                        NumberOfHoles:NumberOfHoles_esMX,
+                        ProductConfiguration:ProductConfiguration_esMX,
+                        IntelligentToilet:IntelligentToilet_esMX,
+                        BowlShape:BowlShape_esMX,
+                        HandleStyle:HandleStyle_esMX,
+                        BidetFunctionality:BidetFunctionality_esMX,
+                        TouchlessFaucet:TouchlessFaucet_esMX,
+                        RegionProductCategoryLocal:RegionProductCategoryLocal_esMX,
+                        RegionSubCategory:RegionSubCategory_esMX
+                    }
+                    response.value[i] = {...response.value[i], ...modifiedObj}
+                  
+                    delete response.value[i]["ProductDescriptionProductShort_esMX"];
+                    delete response.value[i]["ProductLocalCategory_esMX"];
+                    delete response.value[i]["SKUColorFinishName_esMX"];
+                    delete response.value[i]["ProductMaterial_esMX"];
+                    delete response.value[i]["ProductProductType_esMX"];
+                    delete response.value[i]["ProductATGDefaultCategory_esMX"];
+                    delete response.value[i]["ProductSection_esMX"];
+                    delete response.value[i]["ProductInstallationType_esMX"];
+                    delete response.value[i]["ProductShape_esMX"];
+                    delete response.value[i]["SprayType_esMX"];
+                    delete response.value[i]["NightLight_esMX"];
+                    delete response.value[i]["NumberOfHoles_esMX"];
+                    delete response.value[i]["ProductConfiguration_esMX"];
+                    delete response.value[i]["IntelligentToilet_esMX"];
+                    delete response.value[i]["BowlShape_esMX"];
+                    delete response.value[i]["HandleStyle_esMX"];
+                    delete response.value[i]["BidetFunctionality_esMX"];
+                    delete response.value[i]["TouchlessFaucet_esMX"];
+                    delete response.value[i]["RegionProductCategoryLocal_esMX"];
+                    delete response.value[i]["RegionSubCategory_esMX"];
+                    
+                }
             }
+            
             return res.status(200).json({ response:response });
         }catch(error){
             return res.status(500).json({
