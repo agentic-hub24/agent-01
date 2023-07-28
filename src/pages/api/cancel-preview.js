3644d253dbd631ef2495e144d795c067f25b1da5
Afzal Ahmad <AFZAL.AHMAD@kohler.com>
2023-07-29 01:50:58 +0530

    sitemap and robots txt file added

--- src/pages/api/cancel-preview.js
+++ src/pages/api/cancel-preview.js
@@ -1,6 +1,5 @@
 export default function handler(req, res) {
   const { redirect } = req.query;
-  console.log('redirect hee ', redirect);
   res.clearPreviewData();
   res.redirect('/es');
   // res.redirect(encodeURI(redirect) || '/en');
