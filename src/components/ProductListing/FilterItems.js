96b10698b57d59d8855d6195b48278e2a81acb7f
dola.chakraborty@kohler.com <dola.chakraborty@kohler.com>
2023-08-08 15:11:35 +0530

    fix: plp filter with route and pdp bug

--- src/components/ProductListing/FilterItems.js
+++ src/components/ProductListing/FilterItems.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { FaCheck, FaRegSquare } from 'react-icons/fa6';
 import Slider from 'react-slider';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { filterImageFormatter } from './helper';
 
 const FilterItems = ({
@@ -15,6 +15,7 @@ const FilterItems = ({
   selectedFilterCount
 }) => {
   const filter = selectedFilter;
+  const router = useRouter();
   const [isRange, setIsRange] = useState(false);
 
   useEffect(() => {
@@ -30,7 +31,13 @@ const FilterItems = ({
   }, [isRange]);
 
   useEffect(() => {
-    onClickHandler(filterName, filterOption[0]?.value, filterOption[0]?.count);
+    router &&
+      router?.query?.filterBy === filterName &&
+      onClickHandler(
+        filterName,
+        filterOption.find(el => el?.value === router?.query?.value)?.value,
+        filterOption.find(el => el?.value === router?.query?.value)?.count
+      );
   }, []);
 
   const onClickHandler = (name, value, count) => {
