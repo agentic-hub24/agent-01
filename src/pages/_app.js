f11e1475205d1582ffdb5337fa8151e23f72df7a
Ankit Dubey <ANKIT.DUBEY2@kohler.com>
2025-01-03 11:34:52 +0530

    added bootstrap package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Layout from '@components/Layout';
 import ErrorBoundary from '@components/errorBoundary';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../styles/global.scss';
 
 const MyApp = ({ Component, pageProps }) => {
